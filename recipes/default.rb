4e90b083e63b234bf1d7b5c205c537ac591f4bc9
wangjhg <wangjhg@yahoo.com>
2018-03-30 16:26:20 -0400

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+search("users", "platform:#{node["platform"]}").each do |user_data|
+  if node['platform'] == 'windows'
+    user user_data['id'] do
+      comment user_data['comment']
+      action :create
+    end
+  else
+    user user_data['id'] do
+      comment user_data['comment']
+      uid user_data['uid']
+      gid user_data['gid']
+      home user_data['home']
+      shell user_data['shell']
+      action :create
+    end
+  end
+end
